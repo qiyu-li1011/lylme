7dad2d1d7746cb5adcf552f8201e74f0f2f38f03
lizhuo <lizhuo@xcurrency.cn>
2024-10-30 17:35:40 +0800

    init

---
+++ admin/cache.php
@@ -0,0 +1,13 @@
+<?php
+return array (
+  'switch' => true,
+  'title' => '官方公告',
+  'msg_switch' => true,
+  'version' => 'v1.8.5',
+  'update_msg' => '<li class=\'list-group-item\'><b>发现新版本v1.8.5</b> </li><li class=\'list-group-item\'><a class=\'btn btn-success btn-w-md\' href=\'./update.php\' >更新</a></li>',
+  'update_log' => '<li class=\'list-group-item\'><strong>更新版本号1.8.5</strong></li><li class=\'list-group-item\'>优化安装程序，修复安装卡在数据库保存</li><li class=\'list-group-item\'>增加新主题DashLite(基于彩虹工具网修改)</li><li class=\'list-group-item\'>增加导航菜单排序</li><li class=\'list-group-item\'>修复加密组跳转报错</li><li class=\'list-group-item\'>修复随机一言在Windows Server上的错误</li><li class=\'list-group-item\'>增加关于页面后台设置</li><li class=\'list-group-item\'>调整后台侧栏布局</li><li class=\'list-group-item\'>优化5iux主题搜索</li><li class=\'list-group-item\'>增加baisu主题LOGO显示</li><li class=\'list-group-item\'>安全对后台密码加密和修复一个注入漏洞</li><li class=\'list-group-item\'>优化Docker部署</li><li class=\'list-group-item\'>增加对PHP7.x的支持(暂不支持PHP8)</li><li class=\'list-group-item\'>其他的一些优化</li>',
+  'file' => 'https://cdn.lylme.com/lylme_spage/releases/lylme_spage_v1.8.5_update.zip',
+  'msg' => '<li class=\'list-group-item\'>站点授权状态：当前域名[nav.liouy.cn]未授权！当前版本未经鉴权，功能将会受限！</li><li class=\'list-group-item\'>若您已授权，点击<a href=\'./update.php\'>更新</a>下发授权秘钥，然后返回后台首页即可</li><li class=\'list-group-item\'>非常感谢热心网友们对本开源项目的赞助和支持 <a href=\'https://www.lylme.com/support/\' target=\'_blank\'>查看名单</a></li>',
+  'domain' => 'nav.liouy.cn',
+)
+?>
